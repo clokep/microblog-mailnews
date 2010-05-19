d6b6fdeaaecf1547b83e3e848bab546060ec17c0
Patrick Cloke <clokep@gmail.com>
2010-05-19 19:30:40 +0000

    Fixing a mistake in the server object

--- components/twitterServer.js
+++ components/twitterServer.js
@@ -29,7 +29,6 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/JSExtendedUtils.jsm");
 
 const kMaxProtocols = 2;
-const kMaxLoads = 4;
 
 function twitterServer() {
 	this.wrappedJSObject = this;
@@ -59,7 +58,7 @@ twitterServer.prototype = {
 	_protocols: [],
 	runTask: function (task) {
 		if (this._protocols.length < kMaxProtocols) {
-			let protocol = new wfProtocol(this);
+			let protocol = new twitterProtocol(this);
 			protocol.loadTask(task);
 			this._protocols.push(protocol);
 			return;
@@ -77,66 +76,6 @@ twitterServer.prototype = {
 			return this._queuedTasks.shift();
 		return null;
 	},
-
-	// Queued URLs; first kMaxLoads are the currently running
-	_urls: [],
-	// The current task
-	_task: null,
-	// Load the next URL; if all URLs are finished, finish the task
-	onUrlLoaded: function (url) {
-		if (this._urls.length > kMaxLoads)
-			this._urls[kMaxLoads].runUrl();
-		this._urls.shift();
-		if (this._urls.length == 0)
-			this.finishTask();
-	},
-	/**
-	 * Queue the next URL to load.
-	 * Any extra arguments will be passed to the callback method.
-	 * The callback is called with this protocol as the this object.
-	 */
-	loadUrl: function (url, callback) {
-		let closure = this;
-		let task = new UrlRunner(url, this);
-		let argcalls = [null];
-		for (let i = 2; i < arguments.length; i++)
-			argcalls.push(arguments[i]);
-		task.onUrlLoad = function (dom) {
-			argcalls[0] = dom;
-			callback.apply(closure, argcalls);
-		};
-		this._urls.push(task);
-		if (this._urls.length <= kMaxLoads)
-			task.runUrl();
-	},
-	/// Run the task
-	loadTask: function (task) {
-		this._task = task;
-		this._task.runTask(this);
-	},
-	/// Handle a completed task
-	finishTask: function () {
-		let task = this._server.getNextTask();
-		this._task.onTaskCompleted(this);
-		if (task)
-			this.loadTask(task);
-	}
-};
-
-// An object that represents a URL to be run
-function UrlRunner(url, protocol) {
-	this._url = url;
-	this._protocol = protocol;
-}
-UrlRunner.prototype = {
-	runUrl: function () {
-		let real = this;
-		asyncLoadDom(this._url, function (dom) {
-			real.onUrlLoad(dom);
-			real._protocol.onUrlLoaded(real._url);
-		});
-	},
-	onUrlLoad: function (dom) {}
 };
 
 function NSGetModule(compMgr, fileSpec) {
